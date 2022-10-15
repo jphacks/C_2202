bcffa7cda0874d510e2a18338955c0bdd5bebf45
YutaK1026 <89566282+YutaK1026@users.noreply.github.com>
2022-10-15 13:03:24 +0900

    add-ルーティーング周りの設定

---
+++ frontend/src/pages/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Routes, Route  } from 'react-router-dom';
+import StartPage from './StartPage';
+import RadarChartComponent from './RadarChartPage';
+import { MyResponsiveRadar } from './N_RadarChartPage';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path={`/`} element={<StartPage />} />
+            <Route path={`/radar/`} element={<RadarChartComponent />} />
+            <Route path={`/radar_2/`} element={<MyResponsiveRadar />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
