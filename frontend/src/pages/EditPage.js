b181f58241cd04b33b34453e5de0780859fb135b
hirakuuuu <m.hiraku1129@gmail.com>
2022-10-17 16:16:41 +0900

    [update] カラムを自動で追加するように修正 #29

--- frontend/src/pages/EditPage.js
+++ frontend/src/pages/EditPage.js
@@ -8,8 +8,10 @@ import React, {
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import "../styles/startPage.css";
 import "../styles/editPage.css";
 import "../styles/inputModal.css";
+
 import StartButton from "../components/StartButton";
 import MenuBar from "../components/MenuBar";
 import AddButton from "../components/AddButton";
@@ -20,144 +22,132 @@ const backendURL = "http://127.0.0.1:8000";
 
 const EditPage = () => {
   var data = [
-    {
-      商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
-      価格: 31900,
-      評価: 470,
-      画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
-      Brand: "HHKB",
-      メーカー: "PFU",
-      シリーズ: "HYBRID",
-      梱包サイズ: "32.2x16x5.8cm;820g",
-      電池: "2単3形電池(付属)",
-      製造元リファレンス: "PD-KB820B",
-      カラー: "Black",
-      同梱バッテリー: "はい",
-      商品の重量: "820g",
-    },
-    {
-      商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
-      価格: 31900,
-      評価: 470,
-      画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
-      Brand: "HHKB",
-      メーカー: "PFU",
-      シリーズ: "HYBRID",
-      梱包サイズ: "32.2x16x5.8cm;820g",
-      電池: "2単3形電池(付属)",
-      製造元リファレンス: "PD-KB820B",
-      カラー: "Black",
-      同梱バッテリー: "はい",
-      商品の重量: "820g",
-    },
-    {
-      商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
-      価格: 31900,
-      評価: 470,
-      画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
-      Brand: "HHKB",
-      メーカー: "PFU",
-      シリーズ: "HYBRID",
-      梱包サイズ: "32.2x16x5.8cm;820g",
-      電池: "2単3形電池(付属)",
-      製造元リファレンス: "PD-KB820B",
-      カラー: "Black",
-      同梱バッテリー: "はい",
-      商品の重量: "820g",
-    },
-    {
-      商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
-      価格: 31900,
-      評価: 470,
-      画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
-      Brand: "HHKB",
-      メーカー: "PFU",
-      シリーズ: "HYBRID",
-      梱包サイズ: "32.2x16x5.8cm;820g",
-      電池: "2単3形電池(付属)",
-      製造元リファレンス: "PD-KB820B",
-      カラー: "Black",
-      同梱バッテリー: "はい",
-      商品の重量: "820g",
-    },
-    {
-      商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
-      価格: 31900,
-      評価: 470,
-      画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
-      Brand: "HHKB",
-      メーカー: "PFU",
-      シリーズ: "HYBRID",
-      梱包サイズ: "32.2x16x5.8cm;820g",
-      電池: "2単3形電池(付属)",
-      製造元リファレンス: "PD-KB820B",
-      カラー: "Black",
-      同梱バッテリー: "はい",
-      商品の重量: "820g",
-    },
-    {
-      商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
-      価格: 31900,
-      評価: 470,
-      画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
-      Brand: "HHKB",
-      メーカー: "PFU",
-      シリーズ: "HYBRID",
-      梱包サイズ: "32.2x16x5.8cm;820g",
-      電池: "2単3形電池(付属)",
-      製造元リファレンス: "PD-KB820B",
-      カラー: "Black",
-      同梱バッテリー: "はい",
-      商品の重量: "820g",
-    },
-    {
-      商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
-      価格: 31900,
-      評価: 470,
-      画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
-      Brand: "HHKB",
-      メーカー: "PFU",
-      シリーズ: "HYBRID",
-      梱包サイズ: "32.2x16x5.8cm;820g",
-      電池: "2単3形電池(付属)",
-      製造元リファレンス: "PD-KB820B",
-      カラー: "Black",
-      同梱バッテリー: "はい",
-      商品の重量: "820g",
-    },
-    {
-      商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
-      価格: 31900,
-      評価: 470,
-      画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
-      Brand: "HHKB",
-      メーカー: "PFU",
-      シリーズ: "HYBRID",
-      梱包サイズ: "32.2x16x5.8cm;820g",
-      電池: "2単3形電池(付属)",
-      製造元リファレンス: "PD-KB820B",
-      カラー: "Black",
-      同梱バッテリー: "はい",
-      商品の重量: "820g",
-    },
+    // {
+    //   商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
+    //   価格: 31900,
+    //   評価: 470,
+    //   画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
+    //   Brand: "HHKB",
+    //   メーカー: "PFU",
+    //   シリーズ: "HYBRID",
+    //   梱包サイズ: "32.2x16x5.8cm;820g",
+    //   電池: "2単3形電池(付属)",
+    //   製造元リファレンス: "PD-KB820B",
+    //   カラー: "Black",
+    //   同梱バッテリー: "はい",
+    //   商品の重量: "820g",
+    // },
+    // {
+    //   商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
+    //   価格: 31900,
+    //   評価: 470,
+    //   画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
+    //   Brand: "HHKB",
+    //   メーカー: "PFU",
+    //   シリーズ: "HYBRID",
+    //   梱包サイズ: "32.2x16x5.8cm;820g",
+    //   電池: "2単3形電池(付属)",
+    //   製造元リファレンス: "PD-KB820B",
+    //   カラー: "Black",
+    //   同梱バッテリー: "はい",
+    //   商品の重量: "820g",
+    // },
+    // {
+    //   商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
+    //   価格: 31900,
+    //   評価: 470,
+    //   画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
+    //   Brand: "HHKB",
+    //   メーカー: "PFU",
+    //   シリーズ: "HYBRID",
+    //   梱包サイズ: "32.2x16x5.8cm;820g",
+    //   電池: "2単3形電池(付属)",
+    //   製造元リファレンス: "PD-KB820B",
+    //   カラー: "Black",
+    //   同梱バッテリー: "はい",
+    //   商品の重量: "820g",
+    // },
+    // {
+    //   商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
+    //   価格: 31900,
+    //   評価: 470,
+    //   画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
+    //   Brand: "HHKB",
+    //   メーカー: "PFU",
+    //   シリーズ: "HYBRID",
+    //   梱包サイズ: "32.2x16x5.8cm;820g",
+    //   電池: "2単3形電池(付属)",
+    //   製造元リファレンス: "PD-KB820B",
+    //   カラー: "Black",
+    //   同梱バッテリー: "はい",
+    //   商品の重量: "820g",
+    // },
+    // {
+    //   商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
+    //   価格: 31900,
+    //   評価: 470,
+    //   画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
+    //   Brand: "HHKB",
+    //   メーカー: "PFU",
+    //   シリーズ: "HYBRID",
+    //   梱包サイズ: "32.2x16x5.8cm;820g",
+    //   電池: "2単3形電池(付属)",
+    //   製造元リファレンス: "PD-KB820B",
+    //   カラー: "Black",
+    //   同梱バッテリー: "はい",
+    //   商品の重量: "820g",
+    // },
+    // {
+    //   商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
+    //   価格: 31900,
+    //   評価: 470,
+    //   画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
+    //   Brand: "HHKB",
+    //   メーカー: "PFU",
+    //   シリーズ: "HYBRID",
+    //   梱包サイズ: "32.2x16x5.8cm;820g",
+    //   電池: "2単3形電池(付属)",
+    //   製造元リファレンス: "PD-KB820B",
+    //   カラー: "Black",
+    //   同梱バッテリー: "はい",
+    //   商品の重量: "820g",
+    // },
+    // {
+    //   商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
+    //   価格: 31900,
+    //   評価: 470,
+    //   画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
+    //   Brand: "HHKB",
+    //   メーカー: "PFU",
+    //   シリーズ: "HYBRID",
+    //   梱包サイズ: "32.2x16x5.8cm;820g",
+    //   電池: "2単3形電池(付属)",
+    //   製造元リファレンス: "PD-KB820B",
+    //   カラー: "Black",
+    //   同梱バッテリー: "はい",
+    //   商品の重量: "820g",
+    // },
+    // {
+    //   商品名: "PFUキーボードHHKBProfessionalHYBRID日本語配列/墨",
+    //   価格: 31900,
+    //   評価: 470,
+    //   画像: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61ZtNZ4GYCL._AC_SL1280_.jpg",
+    //   Brand: "HHKB",
+    //   メーカー: "PFU",
+    //   シリーズ: "HYBRID",
+    //   梱包サイズ: "32.2x16x5.8cm;820g",
+    //   電池: "2単3形電池(付属)",
+    //   製造元リファレンス: "PD-KB820B",
+    //   カラー: "Black",
+    //   同梱バッテリー: "はい",
+    //   商品の重量: "820g",
+    // },
   ];
 
   const submitRef = useRef(null);
 
   const [dataList, setDataList] = useState(data); // 商品の情報のリスト
-  const [columnList, setColumnList] = useState([
-    "価格",
-    "評価",
-    "Brand",
-    "メーカー",
-    "シリーズ",
-    "梱包サイズ",
-    "電池",
-    "製造元リファレンス",
-    "カラー",
-    "同梱バッテリー",
-    "商品の重量",
-  ]); // カラムのリスト
+  const [columnList, setColumnList] = useState([]); // カラムのリスト
   const [newProductURL, setNewProductURL] = useState("");
   const [showInputModal, setShowInputModal] = useState(false); // Modalコンポーネントの表示の状態を定義する
   const [sort, setSort] = useState({}); // ソートするキーと昇順or降順の状態を保持
@@ -245,17 +235,27 @@ const EditPage = () => {
       .then(function (res) {
         console.log(res.data);
         try {
+          // 商品リストに追加
           let dataList_ = dataList;
           dataList_.push(res.data);
           setDataList(dataList_);
+          // 新しいカラムを追加
+          let newColumnList = columnList;
+          for (const newcol of Object.keys(res.data)) {
+            if (
+              newcol !== "商品名" &&
+              newcol !== "画像" &&
+              !newColumnList.some((col) => newcol === col)
+            ) {
+              newColumnList.push(newcol);
+            }
+          }
+          setColumnList(newColumnList);
         } catch (e) {
           window.alert(e);
         }
         setShowInputModal(false);
       });
-  };
-  const submitClicked = () => {
-    submitRef.current.click();
   };
 
   return (
@@ -344,19 +344,18 @@ const EditPage = () => {
               </div>
               <div className="enter-button">
                 <button
+                  type="submit"
                   className="btn btn-primary"
                   style={{
                     backgroundColor: "#4d638c",
                     color: "#d2d2d2",
                     width: "8rem",
                   }}
-                  onClick={submitClicked}
+                  // onClick={submitClicked}
                 >
                   Enter
                 </button>
               </div>
-
-              <input hidden ref={submitRef} type="submit" value="PDFchange" />
             </form>
           </div>
         </div>
