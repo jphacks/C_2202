1735b5b04d3de7b925b50ccfcf81269673f3df79
hirakuuuu <m.hiraku1129@gmail.com>
2022-10-21 01:57:59 +0900

    [add&fix] csvのアップロード機能を追加&ちょっとリファクタリング #51

--- frontend/src/components/StartButton.js
+++ frontend/src/components/StartButton.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-const StartButton = ({ text, buttonClick }) => {
+const StartButton = (props) => {
+  const { text, onClick, type, width } = props;
   return (
     <div>
       <button
         className="btn btn-primary"
-        style={{ backgroundColor: "#4d638c", color: "#d2d2d2", width: "8rem" }}
-        onClick={buttonClick}
+        style={{ backgroundColor: "#4d638c", color: "#d2d2d2", width: width }}
+        onClick={onClick}
+        type={type}
       >
         {text}
       </button>
